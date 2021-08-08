339dcdeb1c4c226a4324571d6d1bdbb39f5975b7
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2021-08-08 09:41:42 +0700

    Add project files.

---
+++ SK-Lib-math.h-fabs-Bagian1__C/Source.c
@@ -0,0 +1,17 @@
+#include <stdio.h>      /* printf */
+#include <math.h>       /* fabs */
+#include <conio.h>
+
+/*
+	Source by CPlusPlus
+	Modified For Learn by RK
+	I.D.E : VS2019
+*/
+
+int main() {
+	printf("The absolute value of 3.1416 is %f\n", fabs(3.1416));
+	printf("The absolute value of -10.6 is %f\n", fabs(-10.6));
+
+	_getch();
+	return 0;
+}
